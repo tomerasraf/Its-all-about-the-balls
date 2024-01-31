903e255039a0d09234ed2fdd91ff3cc5b6d44fec
Tomer Asraf <tomerasraf2@gmail.com>
2024-01-31 17:32:59 +0700

    + added TouchHightLimit

--- Assets/Scripts/TouchManager.cs
+++ Assets/Scripts/TouchManager.cs
@@ -3,36 +3,51 @@ using UnityEngine;
 public class TouchManager : MonoBehaviour
 {
     [SerializeField] GameObject ballPrefab;
+    [SerializeField] float touchHightLimit = 5.9f;
 
     Vector3 initialTouchPosition;
+    Vector3 currentMousePos;
 
     GameObject ballObject;
     Ball ball;
 
     private void Update()
     {
+        HandleInput();
+    }
+
+    private void HandleInput()
+    {
         if (Input.GetMouseButtonDown(0))
         {
-            initialTouchPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            initialTouchPosition.z = -1.02f;
-
+            ConvertMouseInputFromScreenToWorldView();
             SpawnBallOnTouch();
+        }
+
+        if (Input.GetMouseButton(0))
+        {
+           
         }
 
         if (Input.GetMouseButtonUp(0))
         {
-            Debug.Log("s0");
             ball.ReleaseBall();
         }
     }
 
+    private void ConvertMouseInputFromScreenToWorldView()
+    {
+        initialTouchPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        initialTouchPosition.z = -1.02f;
+    }
+
     void SpawnBallOnTouch()
     {
-        if(initialTouchPosition.y > 5.9f)
+        if (initialTouchPosition.y > touchHightLimit)
         {
-           ballObject = Instantiate(ballPrefab, initialTouchPosition, Quaternion.identity);
+            ballObject = Instantiate(ballPrefab, initialTouchPosition, Quaternion.identity);
             ball = ballObject?.GetComponent<Ball>();
             ball.InitBall();
-        }  
+        }
     }
 }
